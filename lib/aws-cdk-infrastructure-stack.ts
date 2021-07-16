edb1710032a2942b5a1b945ce156212c9e1410e9
Tramaine Gillus <tramaine.gillus@gmail.com>
2021-07-15 21:13:43 -0400

    Add Prettier.

--- lib/aws-cdk-infrastructure-stack.ts
+++ lib/aws-cdk-infrastructure-stack.ts
@@ -1,9 +1,12 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from '@aws-cdk/core'
+import * as ec2 from '@aws-cdk/aws-ec2'
 
 export class AwsCdkInfrastructureStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    super(scope, id, props)
 
-    // The code that defines your stack goes here
+    new ec2.Vpc(this, 'CdkVpc', {
+      maxAzs: 2,
+    })
   }
 }
---
+++ prettier.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'always',
+  printWidth: 80,
+  quoteProps: 'as-needed',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+}
