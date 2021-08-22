5a4a1b6ba0d1f6003028928aea3d9e2a59e91130
Tramaine Gillus <tramaine.gillus@gmail.com>
2021-08-22 14:36:43 -0400

    Add API Gateway stack.

--- bin/aws-cdk-infrastructure.ts
+++ bin/aws-cdk-infrastructure.ts
@@ -4,10 +4,12 @@ import * as cdk from '@aws-cdk/core';
 import { NetworkingStack } from '../lib/stacks/networking-stack';
 import { StorageStack } from '../lib/stacks/storage-stack';
 import { EmployeeBonusStack } from '../lib/stacks/employee-bonus-stack';
+import { ApiGatewayStack } from '../lib/stacks/api-gateway-stack';
 
 const app = new cdk.App();
 new NetworkingStack(app, 'NetworkingStack');
 const { deploymentArtifactsBucket } = new StorageStack(app, 'StorageStack');
+new ApiGatewayStack(app, 'ApiGatewayStack');
 new EmployeeBonusStack(app, 'EmployeeBonusStack', {
   deploymentArtifactsBucket,
 });
